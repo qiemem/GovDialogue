e540368beb83a9b791a71cf5bd8d0c28854810fb
Bryan Head <bryan.head@gmail.com>
2009-09-19 15:26:48 -0700

    Create session has been fixed up.

--- html/govit/lib/sessionmanagement.php
+++ html/govit/lib/sessionmanagement.php
@@ -3,15 +3,9 @@
 require_once("usermanagement.php");
 
 function create_session($id) {
-    $result = get_user_row($id);
-    if($result) {
-	$_SESSION['user'] 
-	    = array('id' => mysql_result($result, 0, 'id'),
-		    'email' => mysql_result($result, 0, 'email'),
-		    'firstname' => mysql_result($result, 0, 'firstname'),
-		    'lastname' => mysql_result($result, 0, 'lastname'),
-		    'canpost' => mysql_result($result, 0, 'canport'),
-		    'validated' => int_to_bool(mysql_result($result, 0, 'validated')));
+    $row = get_user_row($id);
+    if($row) {
+	$_SESSION['user'] = $row;
 	return true;
     }else{
 	return false;
