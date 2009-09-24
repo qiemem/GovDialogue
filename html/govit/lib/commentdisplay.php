a2b0ae9a08717c3cb9a98f6a3ff6a2fe589411ee
Bryan Head <bryan.head@gmail.com>
2009-09-23 21:04:05 -0700

    Refactored some more and made the toggle visibility more standards complient.

--- html/govit/lib/commentdisplay.php
+++ html/govit/lib/commentdisplay.php
@@ -21,52 +21,33 @@ function write_comment_ratings($comment) {
 
 function write_comment_reply_info($comment, $showcomments) {
     $commentid=$comment['id'];
+
+    $toggle_vis = "href=\"javascript:void(0);\" onclick=\"toggleVisibility('$commentid');\"";
+
     echo "<p class=\"commentReply\"> <a href=\"#\">Reply</a>\n";
     if(get_num_children($commentid)>0){
         echo "|\n";
         if(array_key_exists($commentid,$showcomments) and $showcomments[$commentid]){
             echo "<span class=\"hideReplies\" id=\"comment".$commentid."hidereplies\" style=\"display: inline;\">\n";
-            echo "<a href=\"javascript:toggleVisibility('$commentid');\">Hide Replies</a>\n";
+            echo "<a $toggle_vis>Hide Replies</a>\n";
             echo "</span>\n";
             echo "<span class=\"showReplies\" id=\"comment".$commentid."showreplies\" style=\"display: none;\">\n";
-            echo "<a href=\"javascript:toggleVisibility('$commentid');\">Show Replies</a> (".get_num_children($commentid).")\n";
+            echo "<a $toggle_vis>Show Replies</a> (".get_num_children($commentid).")\n";
             echo "</span>\n";
         }else{
             echo "<span class=\"hideReplies\" id=\"comment".$commentid."hidereplies\">\n";
-            echo "<a href=\"javascript:toggleVisibility('$commentid');\">Hide Replies</a>\n";
+            echo "<a $toggle_vis>Hide Replies</a>\n";
             echo "</span>\n";
             echo "<span class=\"showReplies\" id=\"comment".$commentid."showreplies\">\n";
-            echo "<a href=\"javascript:toggleVisibility('$commentid');\">Show Replies</a> (".get_num_children($commentid).")\n";
+            echo "<a $toggle_vis>Show Replies</a> (".get_num_children($commentid).")\n";
             echo "</span>\n";
         }
     }
 }
 
-function write_comment_header($comment) {
+function write_comment_replies($comment, $showcomments) {
     $commentid = $comment['id'];
-    if(!$comment['parent']){
-        echo "<li class=\"toplevelcomment\" id=\"comment$commentid\">\n";
-    }else{
-        echo "<li class=\"childcomment\" id=\"comment$commentid\">\n";
-    }
-}
-
-function write_comment_footer($comment) {
-    $commentid = $comment['id'];
-    echo "<a name=\"id$commentid\"></a>\n";
-    echo "</p>\n</li>\n";
-}
-
-function write_comment_thread($commentid, $showcomments) {
-    validate_comment_id($commentid);
-    $comment = get_comment($commentid);
-    $user = get_user($comment['user']);
-    write_comment_header($comment);
-    write_comment_content($comment);
-    write_comment_credits($comment);
-    write_comment_ratings($comment);
-    write_comment_reply_info($comment,$showcomments);
-    write_comment_footer($comment);
+    echo "<!-- Begin comment $commentid replies -->";
     $children = get_children_of_comment($commentid);
     if(mysql_num_rows($children)>0){
         echo "<li>\n";
@@ -80,6 +61,38 @@ function write_comment_thread($commentid, $showcomments) {
         }
         echo "</ol>\n</li>\n";
     }
+    echo "<!-- End comment $commentid replies -->";
+}
+
+function write_comment_header($comment) {
+    $commentid = $comment['id'];
+    echo "<!-- Begin comment $commentid -->\n";
+    if(!$comment['parent']){
+        echo "<li class=\"toplevelcomment\" id=\"comment$commentid\">\n";
+    }else{
+        echo "<li class=\"childcomment\" id=\"comment$commentid\">\n";
+    }
+}
+
+function write_comment_footer($comment) {
+    $commentid = $comment['id'];
+    echo "<a name=\"id$commentid\"></a>\n";
+    echo "</p>\n</li>\n";
+    echo "<!-- End comment $commentid -->\n";
+}
+
+function write_comment_thread($commentid, $showcomments) {
+    validate_comment_id($commentid);
+    $comment = get_comment($commentid);
+    $user = get_user($comment['user']);
+    write_comment_header($comment);
+    write_comment_content($comment);
+    write_comment_credits($comment);
+    write_comment_ratings($comment);
+    write_comment_reply_info($comment,$showcomments);
+    write_comment_replies($comment,$showcomments);
+    write_comment_footer($comment);
+    
 }
 
 function write_comments_of_post($postid, $showcomments) {
