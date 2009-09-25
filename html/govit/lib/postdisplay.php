c8d105149b2d9c21da0e28d92a5e5d2bce54ba8f
Bryan Head <bryan.head@gmail.com>
2009-09-25 14:51:21 -0700

    Did post stuff.

--- html/govit/lib/postdisplay.php
+++ html/govit/lib/postdisplay.php
@@ -9,19 +9,23 @@ function display_post($postid) {
     $name = $user['firstname']." ".$user['lastname'];
     $tags = $post['tags'];
     echo "<div class=\"postTitle\">\n";
-    echo "<h2>$title<\h2>\n";
+    echo "<h2>$title</h2>\n";
     echo "<p class=\"postDescription\">\n";
     echo "$content\n";
     echo "</p>\n";
     echo "<p class=\"postCredits\">Posted by <a href=\"#\">";
+    echo $name."</a></p>\n";
+    echo "<p class=\"postTags\">Tags: ";
     write_post_tags($tags);
+    echo "</p>\n";
     echo "\n</div>\n";
 }
 
 function write_post_tags($tags) {
-    tags_array = strtok($tags," ");
-    for( $tags_array => $tag ){
-        echo "<a href=\"#\">$tag<\a> ";
+    $tags_array = preg_split("/[\s]+/", $tags);
+
+    foreach($tags_array as $tag){
+        echo "<a href=\"#\">$tag</a> ";
     }
 }
 ?>
--- html/govit/viewpost.php
+++ html/govit/viewpost.php
@@ -1,8 +1,8 @@
 <?php
 
-// viewpost.php
+  // viewpost.php
 error_reporting(E_ALL);
-ini_set('display_errors', true);
+ini_set('display_errors', 1);
 require_once("header.php");
 require_once("lib/commentdisplay.php");
 require_once("lib/postdisplay.php");
@@ -10,34 +10,18 @@ printHeader("Title", "Keywords", "Description");
 
 
 $post_id = null;
-if (isset($_GET
-
-display_post($post_id);
-
-?>
-
-<div class="postTitle">
-    <h2>Post title goes here. This is what a post title would be. It might ask a question, no?</h2>
-    <p class="postDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-    <p class="postCredits">Posted by <a href="#">John Doe</a> on Monday, September 32nd</p>
-    <p class="postTags">Tags: <a href="#">tag one</a>, <a href="#">another tag</a>, <a href="#">third</a></p>
-</div>
-
-<div class="commentsHeader">
-	<h3>Discussion</h3>
-    <p class="discussionsBlurb">Newest comments are posted first.</p>
-</div>
-
-<div class="commentsList">
-  <?php
-    write_comments_of_post(0, array());
-
-    // second parameter: id whose ancestors we want to be shown
-
-  ?>
-</div>
-
-<?php
+if (isset($_GET['postid'])) {
+    $post_id = $_GET['postid'];
+    display_post($post_id);
+    echo "<div class=\"commentsHeader\">\n";
+    echo "<h3>Discussion</h3>\n";
+    echo "<p class=\"discussionsBlurb\">Newest comments are posted first.</p>\n";
+    echo "</div>\n";
+    echo "<div class=\"commentsList\">\n";
+    write_comments_of_post($post_id, array());
+}else{
+    echo "You didn't specify a post.\n";
+}
 
 require_once("footer.php");
 
