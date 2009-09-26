74cfdc2efc2e1b14f8b228df7c81577befecae07
Joey Mornin <joeymornin@gazebocroticon.local>
2009-09-25 19:59:43 -0700

    Created addpost.php

---
+++ html/govit/allposts.php
@@ -0,0 +1,20 @@
+<?php
+
+// allposts.php
+
+require_once("lib/errormanagement.php");
+require_once("lib/postdisplay.php");
+
+// TODO: add meta information
+require_once("header.php");
+printHeader("Add post", "Keywords", "Description");
+
+?>
+
+All posts
+
+<?php
+
+require_once("footer.php");
+
+?>
