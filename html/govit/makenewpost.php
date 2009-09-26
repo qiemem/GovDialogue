73b33f0cfbfaaa8e73dac388702694ef68053da1
Joey Mornin <joeymornin@gazebocroticon.local>
2009-09-25 19:49:28 -0700

    Added javascript to post form

--- html/govit/makenewpost.php
+++ html/govit/makenewpost.php
@@ -20,11 +20,11 @@ if (isUserLoggedIn())
 
 <form name="newPostForm" id="newPostForm" method="POST" action="addpost.php">
 
-    <p><input type="text" name="title" size="50" value="Post title" /></p>
+    <p><input type="text" name="title" size="50" value="Post title" onfocus="if(this.value=='Post title'){this.value='';}" onblur="if(this.value==''){this.value='Post title';}" /></p>
     
-    <p><textarea name="content" id="content" rows="15" cols="70">Write your post here</textarea></p>
+    <p><textarea name="content" id="content" rows="15" cols="70" onfocus="if(this.value=='Write your post here'){this.value='';}" onblur="if(this.value==''){this.value='Write your post here';}">Write your post here</textarea></p>
     
-    <p><input type="tags" name="tags" size="50" /></p>
+    <p><input type="tags" name="tags" size="50" value="Tags (space-separated)" onfocus="if(this.value=='Tags (space-separated)'){this.value='';}" onblur="if(this.value==''){this.value='Tags (space-separated)';}" /></p>
     
     <p><input type="submit" id="submitButton" value="Add Post" name="submitted"/></p>
 
