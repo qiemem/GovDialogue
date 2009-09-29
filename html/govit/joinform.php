9a144c2fed7ae4833a60ade3d93710718ef94d38
Joey Mornin <joeymornin@gazebocroticon.local>
2009-09-29 16:11:40 -0700

    Sexiness for join.php

--- html/govit/joinform.php
+++ html/govit/joinform.php
@@ -6,36 +6,32 @@
 
 <h2>Join</h2>
 
-<form name="joinForm" method="POST" action="<?php $_SERVER["PHP_SELF"] ?>">
+<p class="joinIntro">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
-	<p>
-		First name:
-		<input id="fullname" name="firstname" type="text" />		
-	</p>
-    
-    <p>
-		Last name:
-		<input id="fullname" name="lastname" type="text" />		
-	</p>
-    
-    <p>
-		Email address:
-		<input id="email" name="email" type="text" />		
-	</p>
+<form name="joinForm" id="joinForm" method="POST" action="<?php $_SERVER["PHP_SELF"] ?>">
 
-	<p>
-		Password:
-		<input id="password1" name="password1" type="password" />		
-	</p>
+	<p><input id="firstname" name="firstname" type="text" value="First Name"
+    	onblur="if(this.value==''){this.value='First Name';}this.style.borderColor='#cccccc';"
+        onfocus="if(this.value=='First Name'){this.value='';}this.style.borderColor='#777777';"/></p>
+    
+    <p><input id="firstname" name="lastname" type="text" value="Last Name"
+    	onblur="if(this.value==''){this.value='Last Name';}this.style.borderColor='#cccccc';"
+        onfocus="if(this.value=='Last Name'){this.value='';}this.style.borderColor='#777777';"/></p>
+    
+    <p><input id="email" name="email" type="text" value="Email address"
+    	onblur="if(this.value==''){this.value='Email address';}this.style.borderColor='#cccccc';"
+        onfocus="if(this.value=='Email address'){this.value='';}this.style.borderColor='#777777';"/></p>
+
+	<p><input id="password1" name="password1" type="text" value="Password"
+    	onblur="if(this.value==''){this.value='Password';this.type='text'}this.style.borderColor='#cccccc';if(this.value=='Password'){this.type='text';}"
+        onfocus="if(this.value=='Password'){this.value='';this.type='password'}this.style.borderColor='#777777';"/></p>
 	
-	<p>
-		Confirm password:
-		<input id="password2" name="password2" type="password" />		
-	</p>
+	<p><input id="password2" name="password2" type="text" value="Re-enter your password"
+    	onblur="if(this.value==''){this.value='Re-enter your password';this.type='text'}this.style.borderColor='#cccccc';if(this.value=='Re-enter your password'){this.type='text';}"
+        onfocus="if(this.value=='Re-enter your password'){this.value='';this.type='password'}this.style.borderColor='#777777';"/></p>
 
-	<p>
-		<input type="submit" value="Submit" name="submitted">
-		<input type="reset" value="Reset">
-	</p>
+	<p><input id="tos" name="tos" value="0" type="checkbox" class="checkboxes" /><label for="tos">I have read and agree to the <a href="termsofservice.php">terms of service</label></p>
+
+	<p><input type="submit" class="submitButton" value="Join" name="submitted" /></p>
 
 </form>
